617af2b6c83758abadee27e23108a120521c54fe
Abhijeet-108 <abhijeetofficial.034@gmail.com>
2025-04-25 10:49:38 +0530

    configure redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import {Header, Footer} from './components/index'
+import authService from './appwrite/auth'
+import {login, logout} from "./store/authSlice"
+import { Outlet } from 'react-router-dom'
+
 
 function App() {
   // console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser().then((userData) =>{
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    }).finally(() =>{
+      setLoading(false)
+    })
+  }, [])
 
 
-  return (
-    <>
-      <h1>A blog App with AppWrite</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (null);
 }
 
 export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+    return (
+        
+        <header className="bg-gray-800 text-white py-4">
+            <div className="container mx-auto text-center">
+                <h1 className="text-2xl font-bold">My Blog</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header
